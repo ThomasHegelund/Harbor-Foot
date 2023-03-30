ffb0faa1452ee687601926e1a844d60b838999ab
Eigil121 <eigilalbaek@gmail.com>
2023-03-30 10:12:02 +0200

    My ide needs functions declared before the loop

--- v0.1/Boatsensor_v0.1/Boatsensor_v0.1.ino
+++ v0.1/Boatsensor_v0.1/Boatsensor_v0.1.ino
@@ -21,45 +21,19 @@ const int GREEN_LED_PIN = 2;
 const int RED_LED_PIN = 3;
 const int PHOTORESISTOR_PIN = A0;
 
-
-void setup() {
-  // Setup pins and serial
-   Serial.begin(9600);
-   pinMode(GREEN_LED_PIN, OUTPUT);
-   pinMode(RED_LED_PIN, OUTPUT);
-   pinMode(PHOTORESISTOR_PIN, INPUT);
-   delay(1000);
+bool is_dark(int light_intensity, int threshold = 200)
+{
+  return light_intensity < threshold;
 }
 
-void loop() {
+bool distance_in_range(int distance, int min_distance = 5, int max_distance = 150)
+{
+  return distance > min_distance && distance < max_distance;
+}
 
-  // Check if it is time to make a measurement
-  if (is_time_to_make_measurement()) {
-
-    // Measure the distance with ultrasonic sensor
-    distance = sr04.Distance();
-
-    // If an object is detected between 5 and 150 cm away, the spot is occupied
-    update_occupation_status(distance, 5, 150);
-
-    // If the spot is occupied, turn on the green LED, otherwise turn on red LED
-    update_leds();
-
-    // Measue light intensity
-    light_intensity = analogRead(PHOTORESISTOR_PIN);
-
-    // Switch to nightmode if it is dark
-    update_night_mode(light_intensity)
-
-    // Update time of the latest measure
-    last_measure_time = millis();
-
-    // Print all measurements
-    Serial.print(distance);
-    Serial.println("cm");
-    Serial.print("Light intensity: ")
-    Serial.println(light_intensity);
-    }
+bool is_time_to_make_measurement()
+{
+  return (millis() - last_measure_time) > measure_interval;
 }
 
 void update_night_mode(int light_intensity)
@@ -93,17 +67,46 @@ void update_occupation_status(int distance, int min_distance = 5, int max_distan
   SPOT_OCCUPIED = false;
 }
 
-bool is_dark(int light_intensity, int threshold = 200)
-{
-  return light_intensity < threshold;
+
+
+void setup() {
+  // Setup pins and serial
+   Serial.begin(9600);
+   pinMode(GREEN_LED_PIN, OUTPUT);
+   pinMode(RED_LED_PIN, OUTPUT);
+   pinMode(PHOTORESISTOR_PIN, INPUT);
+   delay(1000);
 }
 
-bool distance_in_range(int distance, int min_distance = 5, int max_distance = 150)
-{
-  return distance > min_distance && distance < max_distance;
+void loop() {
+
+  // Check if it is time to make a measurement
+  if (is_time_to_make_measurement()) {
+
+    // Measure the distance with ultrasonic sensor
+    distance = sr04.Distance();
+
+    // If an object is detected between 5 and 150 cm away, the spot is occupied
+    update_occupation_status(distance, 5, 150);
+
+    // If the spot is occupied, turn on the green LED, otherwise turn on red LED
+    update_leds();
+
+    // Measue light intensity
+    light_intensity = analogRead(PHOTORESISTOR_PIN);
+
+    // Switch to nightmode if it is dark
+    update_night_mode(light_intensity);
+
+    // Update time of the latest measure
+    last_measure_time = millis();
+
+    // Print all measurements
+    Serial.print(distance);
+    Serial.println("cm");
+    Serial.print("Light intensity: ");
+    Serial.println(light_intensity);
+    }
 }
 
-bool is_time_to_make_measurement()
-{
-  return (millis() - last_measure_time) > measure_interval;
-}
+
